30a4e79f60c22fde6da3d6e5f8fe4a73c5f671ec
Guilherme Mendes Rosa <guilhermemr.jlle@gmail.com>
2023-03-11 17:11:21 -0300

    começando API

---
+++ LightWeightAPI/src/main/java/br/com/LightWeightAPI/LightWeightApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.LightWeightAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightWeightApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightWeightApiApplication.class, args);
+	}
+
+}
