d446e1a9adb1921a6bfd9922542b77a5e77be797
Guilherme Mendes Rosa <guilhermemr.jlle@gmail.com>
2023-05-20 19:53:52 -0300

    comecando frontend

---
+++ LightWeightClient/src/app/components/user-workouts/user-workouts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-workouts',
+  templateUrl: './user-workouts.component.html',
+  styleUrls: ['./user-workouts.component.css']
+})
+export class UserWorkoutsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
