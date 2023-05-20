79299ef505c3bafd9910d025f0ed016f343f48fc
Guilherme Mendes Rosa <guilhermemr.jlle@gmail.com>
2023-05-20 20:36:23 -0300

    continuando implementacao

--- LightWeightClient/src/app/app.module.ts
+++ LightWeightClient/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserWorkoutsComponent } from './components/user-workouts/user-workouts.component';
 import { HeaderComponent } from './components/header/header.component';
+import { WorkoutCardComponent } from './components/user-workouts/workout-card/workout-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserWorkoutsComponent,
-    HeaderComponent
+    HeaderComponent,
+    WorkoutCardComponent
   ],
   imports: [
     BrowserModule,
