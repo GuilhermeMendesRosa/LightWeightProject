4fb8f5af1c021228ac530e1b744f8b598a1bcbeb
Guilherme Mendes Rosa <guilhermemr.jlle@gmail.com>
2024-04-26 13:57:22 -0300

    Tirando lógica do controller

--- LightWeightAPI/src/main/java/br/com/LightWeightAPI/controller/WorkoutController.java
+++ LightWeightAPI/src/main/java/br/com/LightWeightAPI/controller/WorkoutController.java
@@ -3,13 +3,8 @@ package br.com.LightWeightAPI.controller;
 import br.com.LightWeightAPI.domain.workout.Workout;
 import br.com.LightWeightAPI.domain.workout.WorkoutDTO;
 import br.com.LightWeightAPI.domain.workout.WorkoutService;
-import br.com.LightWeightAPI.domain.workoutcompound.WorkoutCompound;
-import br.com.LightWeightAPI.domain.workoutcompound.WorkoutCompoundDTO;
 import br.com.LightWeightAPI.infra.utils.LightWeightUtils;
-import com.google.gson.Gson;
 import jakarta.transaction.Transactional;
-import org.hibernate.query.sql.internal.ParameterRecognizerImpl;
-import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,10 +20,6 @@ public class WorkoutController {
 
     @Autowired
     private WorkoutService workoutService;
-
-    @Autowired
-    private OpenAiChatClient chatClient;
-
 
     @PostMapping()
     @Transactional
@@ -77,46 +68,9 @@ public class WorkoutController {
     @RequestMapping("/generate")
     @Transactional
     public ResponseEntity generateWorkout(@RequestBody String description, UriComponentsBuilder uriComponentsBuilder) {
-        Gson gson = new Gson();
-        List<WorkoutCompoundDTO> allWorkoutCompounds = this.workoutService.findAllWorkoutCompounds();
+        WorkoutDTO workoutDTO = this.workoutService.generateWorkoutByAI(description);
 
-        String prompt = String.format("""
-                No meu aplicativo de treinos eu quero que você crie um treino com esse foco: {%s}
-                                
-                Para isso eu quero que você me devolva um json seguindo a estrutura da minha classe WorkoutDTO
-                                
-                WorkoutDTO {
-                                
-                String name;
-                String description;
-                List<WorkoutCompoundDTO> workoutCompoundsDTOs;
-                                
-                }
-                                
-                Ela contém uma lista de WorkoutCompoundDTO dentro dela, essa é a estrutura da classe
-                                
-                WorkoutCompoundDTO {
-                                
-                String exerciseName;
-                Integer series;
-                Integer reps;
-                                
-                }
-                
-                Os WorkoutCompoundDTO disponíveis são esses: {
-                %s
-                }
-                                
-                {{ME RESPONDA SOMENTE COM O JSON EM FORMAT DE STRING}}
-                {{workoutCompoundsDTOs não pode vir vazio use somente os que estão disponíveis, também não pode ter nenhum repetido}
-                """, description, gson.toJson(allWorkoutCompounds));
-
-        String json = chatClient.call(prompt);
-        WorkoutDTO workoutDTO = gson.fromJson(json, WorkoutDTO.class);
-
-        Workout workout = this.workoutService.create(workoutDTO);
-
-        URI uri = uriComponentsBuilder.path("/workout/{id}").buildAndExpand(workout.getId()).toUri();
+        URI uri = uriComponentsBuilder.path("/workout/{id}").buildAndExpand(workoutDTO.getId()).toUri();
 
         return ResponseEntity.created(uri).body(workoutDTO);
     }
--- LightWeightAPI/src/main/java/br/com/LightWeightAPI/domain/workout/WorkoutService.java
+++ LightWeightAPI/src/main/java/br/com/LightWeightAPI/domain/workout/WorkoutService.java
@@ -6,6 +6,8 @@ import br.com.LightWeightAPI.domain.workoutcompound.WorkoutCompound;
 import br.com.LightWeightAPI.domain.workoutcompound.WorkoutCompoundDTO;
 import br.com.LightWeightAPI.domain.workoutcompound.WorkoutCompoundRepository;
 import br.com.LightWeightAPI.infra.utils.LightWeightUtils;
+import com.google.gson.Gson;
+import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +25,9 @@ public class WorkoutService {
 
     @Autowired
     private ExerciseService exerciseService;
+
+    @Autowired
+    private OpenAiChatClient chatClient;
 
     public Workout create(WorkoutDTO workoutDTO) {
         Workout workout = new Workout();
@@ -102,6 +107,49 @@ public class WorkoutService {
 
     public List<WorkoutCompoundDTO> findAllWorkoutCompounds() {
         return this.workoutCompoundRepository.findAll().stream().map(WorkoutCompoundDTO::new).toList();
+    }
+
+    public WorkoutDTO generateWorkoutByAI(String description) {
+        Gson gson = new Gson();
+        List<WorkoutCompoundDTO> allWorkoutCompounds = this.findAllWorkoutCompounds();
+
+        String prompt = String.format("""
+                No meu aplicativo de treinos eu quero que você crie um treino com esse foco: {%s}
+                                
+                Para isso eu quero que você me devolva um json seguindo a estrutura da minha classe WorkoutDTO
+                                
+                WorkoutDTO {
+                                
+                String name;
+                String description;
+                List<WorkoutCompoundDTO> workoutCompoundsDTOs;
+                                
+                }
+                                
+                Ela contém uma lista de WorkoutCompoundDTO dentro dela, essa é a estrutura da classe
+                                
+                WorkoutCompoundDTO {
+                                
+                String exerciseName;
+                Integer series;
+                Integer reps;
+                                
+                }
+                                
+                Os WorkoutCompoundDTO disponíveis são esses: {
+                %s
+                }
+                                
+                {{ME RESPONDA SOMENTE COM O JSON EM FORMAT DE STRING}}
+                {{workoutCompoundsDTOs não pode vir vazio use somente os que estão disponíveis, também não pode ter nenhum repetido}
+                """, description, gson.toJson(allWorkoutCompounds));
+
+        String json = chatClient.call(prompt);
+        WorkoutDTO workoutDTO = gson.fromJson(json, WorkoutDTO.class);
+        Workout workout = this.create(workoutDTO);
+        workoutDTO.setId(workout.getId());
+
+        return workoutDTO;
     }
 
 }
